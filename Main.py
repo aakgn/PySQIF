8cb8b6145b82fa84d91e31d42d5b772d59f604f1
aakgn <75807351+aakgn@users.noreply.github.com>
2021-09-16 09:11:27 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,21 @@
+#############################################################################################
+# @author: ali.akgun
+# @date: 16.09.2021
+# @brief:
+#  Main python file of PySQUID
+# @to-do:
+#  Should make improvements on Bi-SQUID voltage response visualization. Visualization of
+#  voltage response is not smooth due to differential equations solver algorithm.(i think)
+#  You should run this simulator and see what is best !!! :)
+#  You should fix Bi-SQUID period problem !!!
+#  You should create clasesses for output data generation and visualization !!!
+#  You should start to generate graphical user interface !!!
+#  You should integrate SQUID classes into the PySQUID project. (problem is Bi-SQUID time
+#  based and SQUID is static, you can do SQUID as time based and you can use same classes
+#  for both SQUID and Bi-SQUID.)
+# @bugs: There is no known bugs.
+##############################################################################################
+
 from PhiExt import *
 from Voltage import *
 from Period import *
@@ -17,5 +35,3 @@ PhiExtArray = PhiExt(initial, final, data_points)
 PhiExtArray = PhiExtArray.calculate(initial, final, data_points)
 voltage = Voltage(initial_conditions, t, PhiExtArray, ib, l, ic3, data_points)
 voltage.plot(initial_conditions, t, PhiExtArray, ib, l, ic3, data_points)
-    
-    
