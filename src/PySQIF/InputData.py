3296561000fe39d94a3864757475fb872ecc416e
aakgn <75807351+aakgn@users.noreply.github.com>
2021-12-22 20:51:40 +0300

    Add files via upload

--- src/PySQIF/InputData.py
+++ src/PySQIF/InputData.py
@@ -9,6 +9,7 @@
 
 import pandas as pd
 
+VALUES = 1
 class InputData:
  
     # @ali.akgun
@@ -37,24 +38,24 @@ class InputData:
     def initialize(self, input_file):
         
         input_data = pd.read_csv(input_file)
-        psi_initial = [input_data.values[0, 1], input_data.values[1, 1]]
-        magnetic_field_range = [input_data.values[2, 1], input_data.values[3, 1]]
-        magnetic_field_resolution = input_data.values[4, 1]
-        characteristic_voltage = input_data.values[5, 1] 
-        ib = input_data.values[6, 1]
-        ic3 = input_data.values[7, 1]
-        time_resolution = input_data.values[8, 1]
-        beta = input_data.values[9, 1]
-        l1a = input_data.values[10, 1]
-        l1b = input_data.values[11, 1]
-        l2a = input_data.values[12, 1]
-        l2b = input_data.values[13, 1]
-        l3a = input_data.values[14, 1]
-        l3b = input_data.values[15, 1]
-        multiple_run = input_data.values[16, 1]         
-        standard_deviation = input_data.values[17, 1] 
-        mean = input_data.values[18, 1]
-        number_of_runs = input_data.values[19, 1] 
+        psi_initial = [input_data.values[0, VALUES], input_data.values[1, VALUES]]
+        magnetic_field_range = [input_data.values[2, VALUES], input_data.values[3, VALUES]]
+        magnetic_field_resolution = input_data.values[4, VALUES]
+        characteristic_voltage = input_data.values[5, VALUES] 
+        ib = input_data.values[6, VALUES]
+        ic3 = input_data.values[7, VALUES]
+        time_resolution = input_data.values[8, VALUES]
+        beta = input_data.values[9, VALUES]
+        l1a = input_data.values[10, VALUES]
+        l1b = input_data.values[11, VALUES]
+        l2a = input_data.values[12, VALUES]
+        l2b = input_data.values[13, VALUES]
+        l3a = input_data.values[14, VALUES]
+        l3b = input_data.values[15, VALUES]
+        multiple_run = input_data.values[16, VALUES]         
+        standard_deviation = input_data.values[17, VALUES] 
+        mean = input_data.values[18, VALUES]
+        number_of_runs = input_data.values[19, VALUES] 
         
         return [psi_initial, magnetic_field_range, magnetic_field_resolution,\
                 characteristic_voltage, ib, ic3, time_resolution, beta, l1a,
